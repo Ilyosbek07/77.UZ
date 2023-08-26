1b129d9aa45e7bea6dd5111dc5bec51fd0b27416
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-08-26 15:10:22 +0500

    feat:changedd

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from apps.store.models import Category
 
 
 class BaseModel(models.Model):
--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from .schema import swagger_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("apps.users.urls")),
     path("store/", include("apps.store.urls"))
 ]
 
