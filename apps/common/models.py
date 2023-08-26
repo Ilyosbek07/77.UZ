665979e3e8036525c2de5457d36c76a4e29d4b79
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-08-26 12:29:25 +0500

    feat: users and common models written

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class ApplicateUser(models.Model):
+    STATUS = (
+        ('active', 'Active'),
+        ('moderation', 'Moderation'),
+        ('cancelled', 'Cancelled'),
+    )
+
+    full_name = models.CharField(max_length=600)
+    project_name = models.CharField(max_length=600)
+    category = models.ForeignKey()
+    phone_number = models.CharField(max_length=600)
+    address = models.TextField()
+    status = models.CharField(choices=STATUS, max_length=300)
+
+    def __str__(self):
+        return self.full_name
